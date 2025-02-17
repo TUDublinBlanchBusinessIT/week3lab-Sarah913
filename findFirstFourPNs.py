131f79d7edf0728e2e81f12363fadaf2619576d2
Sarah913 <B00161073@mytudublin.ie>
2025-02-17 17:46:29 +0000

    Add files via upload

--- findFirstFourPNs.py
+++ findFirstFourPNs.py
@@ -1,6 +1,21 @@
+#Sarah Forde
+#17/02/2025
+#findFirstFourPNs.py
+
 #Write a program here that finds the first four perfect numbers.
-#Put your name and date at the top of the file.
+
 #The pseudocode in the README.md file will help you write this program.
 
+from perfectNumber import isPerfectNumber
+
+def findFisrtFourNumbers ():
+    count = 0
+    num = 1
+    while count < 4:
+        if isPerfectNumber (num):
+            print(f"{num} is a perfect number!")
+            count += 1
+        num += 1    
 
 
+findFisrtFourNumbers ()
